b5e8e0d87f370ab8578fec3e9f9241645f062d91
nikhilgowda555 <goniku65@gmail.com>
2026-02-11 12:58:12 +0530

    Update caluclator.py

--- caluclator.py
+++ caluclator.py
@@ -1,1 +1,23 @@
+print("simple caluclator")
 
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: ))
+
+print("Choose operation: ")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+if choice == "1":
+  print("Result:", a + b)
+elif choice == "2":
+  print("Result:", a - b)
+elif choice == "3":
+  print("Result:", a * b)
+elif choice == "4":
+  print("Result:", a / b)
+else:
+  print("Invalid choice")
