da3881c0086901130d9a29cbdbc7ef00b2cde0a2
nikhilgowda555 <goniku65@gmail.com>
2026-02-11 13:10:35 +0530

    Update caluclator.py

--- caluclator.py
+++ caluclator.py
@@ -1,7 +1,7 @@
 print("simple caluclator")
 
 num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number": ))
+num2 = float(input("Enter second number: " ))
 
 print("Choose operation: ")
 print("1. Addition")
